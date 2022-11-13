61c195c446908864bb6e9e2a073afcc02f660285
Jonatán Paes <jonatan.paes@digitalpages.com.br>
2022-11-13 20:30:54 -0300

    Validando formulário

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -32,7 +32,12 @@ export function Post({ author, content, publishedAt }) {
   }
 
   function handleNewCommentChange() {
+    event.target.setCustomValidity("");
     setNewCommentText(event.target.value);
+  }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity("Esse campo é obrigatório");
   }
 
   function deleteComment(commentToDelete) {
@@ -41,6 +46,8 @@ export function Post({ author, content, publishedAt }) {
     });
     setComments(commentsWthoutDeletedOne);
   }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
 
   return (
     <article className={styles.post}>
@@ -84,10 +91,14 @@ export function Post({ author, content, publishedAt }) {
           placeholder="Escreva um comentário..."
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type="submit">Publicar</button>
+          <button type="submit" disabled={isNewCommentEmpty}>
+            Publicar
+          </button>
         </footer>
       </form>
 
